b289c9ae6ea90acf91708cf5fbb0d59f615affc1
PC <maxgit222@mail.ru>
2022-02-13 15:12:42 +0300

    ab

---
+++ src/main/java/com/example/lab1/HelloController.java
@@ -0,0 +1,28 @@
+package com.example.lab1;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome!");
+    }
+
+    @FXML
+    protected void onButHiButtonClick() {welcomeText.setText("Hehe");}
+
+    @FXML
+    private Label Lab2;
+
+    @FXML
+    private void initialize() {
+        Lab2.setText("New text for label without any button");
+    }
+
+
+}
